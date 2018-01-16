d8d4b5caba639fabc91a3722aa5cec5579628661
xerxes <346287244@qq.com>
2018-01-16 16:23:19 +0800

    init

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Helpdesk Sale_Order",
+
+    'summary': """
+    associate helpdesk with sale_order
+    """,
+    'description': """
+    """,
+    'author': "Shine IT",
+    'website': "http://www.odoo.com.cn",
+    'category': 'helpdesk',
+    'version': '0.1',
+    'depends': ['helpdesk'],
+    'data': [
+        'views/helpdesk.xml',
+    ],
+    'currency': 'EUR',
+    'price': 0,
+    'images': ['static/description/helpdesk.jpg']
+}
---
+++ models/helpdesk.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import logging
+from odoo import models, fields, api, tools, _
+
+_logger = logging.getLogger(__name__)
+_test_logger = logging.getLogger('odoo.tests')
+
+class HelpdeskTicket(models.Model):
+    _inherit = 'helpdesk.ticket'
+    sale_order_id = fields.Many2one('sale.order',string='销售订单编号')
+
+    @api.onchange('partner_id')
+    def onchange_so_ids(self):
+        res = {}
+        res['domain'] = {'sale_order_id': [('partner_id', '=', self.partner_id.id)]}
+        return res
+    
